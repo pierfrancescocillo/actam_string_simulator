788760553abc5b349a6760945c645a3dbcefc58f
Pier_Cillo <pierfrancesco.cillo@mail.polimi.it>
2020-12-29 16:58:04 +0100

    made sliders work and other stuff

--- src/index.js
+++ src/index.js
@@ -1,32 +1,4 @@
-/*function Discretize(){
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.beginPath();
-  var i;
-  var ajX= [];
-  var ajY=[];
-  ajX[0]=DPointsX[0];
-  ajY[0]= DPointsY[0];
-  for(i=1; i<DPointsX.length; i++){ 
-  var distance= Math.round(Math.sqrt((DPointsX[i]-DPointsX[i-1])^2+(DPointsY[i]-DPointsY[i-1])^2));
-  if(distance!=2){
-    var NewX=Math.round(Math.sqrt((distance^2)-(DPointsY[i]-DPointsY[i-1])^2));    
-    ajX.push(NewX);
-  } else {
-    ajX.push(DPointsX[i])
-  }
-    ctx.fillRect(ajX[i], ajY[i], 2,2);
-  }
-ctx.fillStyle= "red";
-ctx.stroke();
-ctx.closePath();
-    
-}*/
-
-
-
-
 console.clear();
-//import lineInterpolatePoints from 'https://cdn.skypack.dev/line-interpolate-points@1.0.5';
 
 var canvas = document.getElementById("can");
 
@@ -74,49 +46,48 @@ function mouseMove (e) {
   KeepTrack(currX,currY);
         };
 };
-  function mouseDown (e) {
+  
+function mouseDown (e) {
     if(complete) return;
      startDrawing=true;
- };
+};
 
-    function draw() {
-        if(move && startDrawing){         
-           ctx.beginPath();
-           if(empty){
-           prevX=currX;
-           prevY=currY;
-           currX=mintol;
-           currY=height/2;
-           ctx.moveTo(prevX, prevY);
-           ctx.lineTo(currX, currY);
-
-           empty=false;
-           } 
-           else if (prevX>maxtol)
-          { 
-            ctx.moveTo(prevX, prevY);
-            ctx.lineTo(width, height/2);
-            complete=true;
-            //console.log(points)     
-          }
-          
-          
-          else if(currX<prevX){
-            currX=prevX;
-            currY=prevY;
-          }
-          else {
-           ctx.moveTo(prevX, prevY);
-           ctx.lineTo(currX, currY);
-          }
-                                           ctx.lineWidth=2;
-        ctx.lineCap="round";
-        ctx.lineJoin="round";
-        ctx.stroke();
-        ctx.closePath();
-        }
-
+function draw() {
+  if(move && startDrawing){         
+    ctx.beginPath();
+    if(empty){
+    prevX=currX;
+    prevY=currY;
+    currX=mintol;
+    currY=height/2;
+    ctx.moveTo(prevX, prevY);
+    ctx.lineTo(currX, currY);
+    empty=false;
+    } 
+    else if (prevX>maxtol)
+    { 
+      ctx.moveTo(prevX, prevY);
+      ctx.lineTo(width, height/2);
+      complete=true;
+      //console.log(points)     
     }
+    else if(currX<prevX){
+      currX=prevX;
+      currY=prevY;
+    }
+    else {
+      ctx.moveTo(prevX, prevY);
+      ctx.lineTo(currX, currY);
+    }
+    ctx.lineWidth=2;
+    ctx.lineCap="round";
+    ctx.lineJoin="round";
+    ctx.stroke();
+    ctx.closePath();
+  }
+
+}
+
 function DrawPoints(NewPoints){
   ctx.clearRect(0,0,width,height)
   ctx.beginPath();
@@ -135,8 +106,9 @@ function ChangeYcord(NewPoints){
     NewPoints[i][1]=(height/2)-NewPoints[i][1]
   }
 };
+
 function Discretize(){
-  var interpolateLineRange = require( 'line-interpolate-points' );
+var interpolateLineRange = require( 'line-interpolate-points' );
  NewPoints = interpolateLineRange(points, 120)
  startAnim = true;
  DrawPoints(NewPoints);
@@ -144,39 +116,27 @@ function Discretize(){
  //console.log(NewPoints);
 };
 
- //---------------------------------------------------------------------
-  //constant definitions
-  var T = document.getElementById("sl_T").value; //
-  const L = document.getElementById("sl_L").value; //0.65;
-  const m = document.getElementById("sl_m").value; //2.33 * Math.pow(10,-3);
-  const inte = 50;
-  const nu = document.getElementById("sl_nu").value; //= (2 * m * 0.5)*5;
-  //const nu = (2*Math.PI*Math.sqrt(T*m)) / L +0.1
-  //const nu = 0;
-  const i_bar = nu*L/(2*Math.PI*Math.sqrt(T*m));
-  const alpha = nu/(2*m);
+//Assigning value to the sliders------------------------------------------------------------
+document.getElementById("T_value").innerHTML = document.getElementById("sl_T").value;
+document.getElementById("sl_T").oninput = function(){
+  document.getElementById("T_value").innerHTML = this.value;
+}
 
-  //Assigning value to the sliders
-  document.getElementById("T_value").innerHTML = document.getElementById("sl_T").value;
-  document.getElementById("sl_T").oninput = function(){
-    document.getElementById("T_value").innerHTML = this.value;
-  }
+document.getElementById("L_value").innerHTML = document.getElementById("sl_L").value;
+document.getElementById("sl_L").oninput = function(){
+  document.getElementById("L_value").innerHTML = this.value;
+}
 
-  document.getElementById("L_value").innerHTML = document.getElementById("sl_L").value;
-  document.getElementById("sl_L").oninput = function(){
-    document.getElementById("L_value").innerHTML = this.value;
-  }
+document.getElementById("m_value").innerHTML = document.getElementById("sl_m").value;
+document.getElementById("sl_m").oninput = function(){
+  document.getElementById("m_value").innerHTML = this.value;
+}
 
-  document.getElementById("m_value").innerHTML = document.getElementById("sl_m").value;
-  document.getElementById("sl_m").oninput = function(){
-    document.getElementById("m_value").innerHTML = this.value;
-  }
-
-  document.getElementById("nu_value").innerHTML = document.getElementById("sl_nu").value;
-  document.getElementById("sl_nu").oninput = function(){
-    document.getElementById("nu_value").innerHTML = this.value;
-  }
-  //--------------------------------------------------------------------
+document.getElementById("nu_value").innerHTML = document.getElementById("sl_nu").value;
+document.getElementById("sl_nu").oninput = function(){
+  document.getElementById("nu_value").innerHTML = this.value;
+}
+//--------------------------------------------------------------------
 
 //  ------------ANIMATION CODE--------------- 
 function Animation(){
@@ -279,6 +239,20 @@ function Animation(){
 //---------ANIMATION DAMPED CODE--------------------
 function Animation_damped(){
 
+  document.getElementById("can").style.cursor = "default";
+
+  //constant definitions----------------------------------------------------------
+  var T = document.getElementById("sl_T").value; //
+  const L = document.getElementById("sl_L").value; //0.65;
+  const m = document.getElementById("sl_m").value; //2.33 * Math.pow(10,-3);
+  const inte = 50;
+  const nu = document.getElementById("sl_nu").value; //= (2 * m * 0.5)*5;
+  //const nu = (2*Math.PI*Math.sqrt(T*m)) / L +0.1
+  //const nu = 0;
+  const i_bar = nu*L/(2*Math.PI*Math.sqrt(T*m));
+  const alpha = nu/(2*m);
+  //--------------------------------------------------------------------------------
+
   const n_points = NewPoints.length;
   //console.clear();
   //console.log(NewPoints);
@@ -360,6 +334,7 @@ function Animation_damped(){
   var cosh = Array(n_modes).fill(0);
 
   console.log("starting animation...");
+  document.getElementById("buffering").style.display = "none";
   var time=0;
   setInterval(goOn,inte);
 
@@ -404,7 +379,11 @@ function Animation_damped(){
     
 }
 
-animate.onclick = function() {if(startAnim){Animation_damped()}};
+animate.onclick = function() {if(startAnim){
+  document.getElementById("buffering").style.display = "inline-block";
+  Animation_damped();
+  }
+};
 
 //lazy functions
 function getCol(matrix, col){
